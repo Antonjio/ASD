16afbbf9bc75420e9d8ee643e782ac92d2d4c7b4
Antonjio <antonio.capasso005@studenti.uniparthenope.it>
2025-07-08 12:27:23 +0200

    topological print 2.0

---
+++ Grafi/Topologico 2.0/cmake-build-debug/CMakeFiles/Topologico_2_0.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Topologico_2_0.dir/main.cpp.o"
+  "CMakeFiles/Topologico_2_0.dir/main.cpp.o.d"
+  "Topologico_2_0"
+  "Topologico_2_0.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Topologico_2_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
