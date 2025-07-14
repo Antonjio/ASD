70a75051506d144394edc5557eebf455245335dd
Antonjio <antonio.capasso005@studenti.uniparthenope.it>
2025-07-14 17:14:11 +0200

    Peppe

---
+++ Grafi/Peppe/cmake-build-debug/CMakeFiles/Peppe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Peppe.dir/main.cpp.o"
+  "CMakeFiles/Peppe.dir/main.cpp.o.d"
+  "Peppe"
+  "Peppe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Peppe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
